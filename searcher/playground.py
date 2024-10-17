201c0aad5d524338443ad2d8c1c2184d2588569f
zwickvitaly <zwickvitaly@gmail.com>
2024-10-17 23:10:27 +0300

    no_limits

--- searcher/playground.py
+++ searcher/playground.py
@@ -20,7 +20,7 @@ async def get_cities_data():
 
 async def get_requests_data():
     async with async_session_maker() as s:
-        q = await s.execute(select(Request).order_by(Request.quantity.desc()).limit(1000))
+        q = await s.execute(select(Request).order_by(Request.quantity.desc()))
         requests = q.scalars()
     return requests
 
