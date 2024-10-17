1cfe4ac73d9477bfe434a5462486d09f0c5eda71
zwickvitaly <zwickvitaly@gmail.com>
2024-10-17 23:01:07 +0300

    zeroes

--- searcher/main.py
+++ searcher/main.py
@@ -4,4 +4,4 @@ import uvicorn
 app = app
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info", workers=4)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", workers=4)
