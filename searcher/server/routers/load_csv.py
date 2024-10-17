f43a1fec9034a550032da03e83d1a0eed61fb6c7
zwickvitaly <zwickvitaly@gmail.com>
2024-10-17 23:05:31 +0300

    cities

--- searcher/server/routers/load_csv.py
+++ searcher/server/routers/load_csv.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, File, UploadFile, BackgroundTasks
+from fastapi.params import Body
 from fastapi.responses import JSONResponse
 
+from db import City
+from db.base import async_session_maker
 from server.funcs.prepare_csv_contents import prepare_csv_contents
 from server.funcs.upload_requests_data import upload_requests_csv_bg
 from settings import logger
@@ -21,5 +24,20 @@ async def upload_csv(background_tasks: BackgroundTasks, file: UploadFile = File(
         content={
             "message": "CSV uploaded to background.", "error_rows": error_rows
         },
+        status_code=201
+    )
+
+@csv_router.post("/add_cities")
+async def upload_csv(cities: list[dict] = Body()):
+    try:
+        async with async_session_maker() as session:
+            for city in cities:
+                session.add(City(name=city.get("name"), dest=city.get("dest")))
+            await session.commit()
+    except Exception as e:
+        logger.error(f"{e}")
+        return {"message": "Error with cities"}
+    return JSONResponse(
+        content="ok",
         status_code=201
     )
