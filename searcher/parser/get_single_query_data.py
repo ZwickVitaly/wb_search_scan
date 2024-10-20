6a7843c59847f37288b24526dfaa83fa81dc686a
zwickvitaly <zwickvitaly@gmail.com>
2024-10-20 23:33:00 +0300

    play

--- searcher/parser/get_single_query_data.py
+++ searcher/parser/get_single_query_data.py
@@ -6,7 +6,7 @@ from aiohttp import ClientSession, ContentTypeError, client_exceptions, ClientTi
 from settings import SEARCH_URL, logger
 
 
-async def get_query_data(http_session, query_string, dest, limit, page, rqa=5, timeout=5):
+async def get_query_data(http_session: ClientSession, query_string, dest, limit, page, rqa=5, timeout=5):
     _data = {}
     counter = 0
     while (not _data.get("data") or len(_data.get("data").get("products")) < 2) and counter < rqa:
@@ -22,7 +22,7 @@ async def get_query_data(http_session, query_string, dest, limit, page, rqa=5, t
                     "dest": dest,
                     "page": page,
                 },
-                timeout=timeout
+                timeout=timeout,
             ) as response:
                 if response.ok:
                     try:
