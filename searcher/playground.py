55e20ef88f62c5d28683f4d9e244919b992be0c6
zwickvitaly <zwickvitaly@gmail.com>
2024-10-20 23:19:19 +0300

    play

--- searcher/playground.py
+++ searcher/playground.py
@@ -88,7 +88,7 @@ async def get_city_result(city, date):
     prev = 0
     async with ClientSession() as http_session:
         for _ in range(0, len(requests) + 250, 250):
-            logger.critical(f"{city.name} ЧТО ЗА ФИГНЯ {len(requests[prev:_])}")
+            logger.critical(f"ЧТО ЗА ФИГНЯ {prev} - {_}")
             tasks = [asyncio.create_task(get_r_data(r=r, city=city, date=date, http_session=http_session)) for r in requests[prev:_]]
             logger.info(len(tasks))
             rp = await asyncio.gather(*tasks)
