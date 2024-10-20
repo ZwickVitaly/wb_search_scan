eeceb33ada7bf1f2ee0dcadf3f7f6f39de7dc4d6
zwickvitaly <zwickvitaly@gmail.com>
2024-10-20 23:04:04 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -2,13 +2,13 @@ from sqlalchemy import select, func
 import asyncio
 from db import RequestProduct, Request
 from db.base import async_session_maker
+from settings import logger
 
 
 async def check():
     async with async_session_maker() as session:
-        rqs = await session.execute(select(RequestProduct))
-        result = rqs.scalars()
-    for i in result:
-        print(len(i.products))
+        rqs = await session.execute(select(func.count()).select_from(RequestProduct))
+        result = rqs.scalar_one()
+    logger.info(result)
 
 asyncio.run(check())
