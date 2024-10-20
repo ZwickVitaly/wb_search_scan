2d13d94b9097773e97223ef1a682e3abf73cc416
zwickvitaly <zwickvitaly@gmail.com>
2024-10-21 00:53:07 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -7,7 +7,7 @@ from settings import logger
 
 async def check():
     async with async_session_maker() as session:
-        rqs = await session.execute(select(RequestProduct).filter(func.cardinality(RequestProduct.products) <= 2))
+        rqs = await session.execute(select(RequestProduct))
         result = rqs.scalars()
     for r in result:
         logger.info(r.query)
