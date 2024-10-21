7778e0256f76e38e5683b7ed9cea321525a79b69
zwickvitaly <zwickvitaly@gmail.com>
2024-10-21 15:41:38 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -11,22 +11,22 @@ async def check(wb_id):
     async with async_session_maker() as session:
         rqs = await session.execute(select(RequestProduct).filter(RequestProduct.products.contains([wb_id])))
         result = rqs.scalars()
-    moscow = {}
-    krasnodar = {}
-    ekat = {}
-    vlad = {}
+    moscow = set()
+    krasnodar = set()
+    ekat = set()
+    vlad = set()
     for r in result:
         for p, i in zip(r.products, r.positions):
             if p == wb_id:
                 if r.city == 1:
-                    moscow[r.query] = {"date": str(r.date), "place": i}
+                    moscow.add(r.query)
                 elif r.city == 2:
-                    krasnodar[r.query] = {"date": str(r.date), "place": i}
+                    krasnodar.add(r.query)
                 elif r.city == 3:
-                    ekat[r.query] = {"date": str(r.date), "place": i}
+                    ekat.add(r.query)
                 elif r.city == 4:
-                    vlad[r.query] = {"date": str(r.date), "place": i}
-    res = [{"Москва":moscow}, {"Краснодар":krasnodar}, {"Екатеринбург":ekat}, {"Владивосток":vlad}]
+                    vlad.add(r.query)
+    res = [{"Москва":list(moscow)}, {"Краснодар":list(krasnodar)}, {"Екатеринбург":list(ekat)}, {"Владивосток":list(vlad)}]
     print(json.dumps(res, indent=2, ensure_ascii=False))
 
 
