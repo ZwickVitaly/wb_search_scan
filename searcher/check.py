d1f45b9093097b30483b7572e1d3a9871f057bc8
zwickvitaly <zwickvitaly@gmail.com>
2024-10-21 11:14:27 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -7,8 +7,9 @@ from settings import logger
 
 async def check():
     async with async_session_maker() as session:
-        rqs = await session.execute(select(func.count()).select_from(RequestProduct))
-        result = rqs.scalar_one()
-    logger.info(result)
+        rqs = await session.execute(select(RequestProduct))
+        result = rqs.scalars()
+    for r in result:
+        logger.info(f"{r.query} - {len(r.products)}")
 
 asyncio.run(check())
