8df81b7a550ea441c5bdfa47166a8c7a5bf4b7b0
zwickvitaly <zwickvitaly@gmail.com>
2024-10-21 15:04:33 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -7,7 +7,7 @@ from settings import logger
 
 async def check():
     async with async_session_maker() as session:
-        rqs = await session.execute(select(RequestProduct).order_by(func.cardinality(RequestProduct.products).desc().limit(10)))
+        rqs = await session.execute(select(RequestProduct).order_by(func.cardinality(RequestProduct.products).desc()).limit(10))
         result = rqs.scalars()
     for r in result:
         if len(r.products) < 10:
