0e22c28c535ea44127119b8f934f66be8da829c4
zwickvitaly <zwickvitaly@gmail.com>
2024-10-21 18:26:25 +0300

    play

--- searcher/purge.py
+++ searcher/purge.py
@@ -2,13 +2,16 @@ import asyncio
 
 from sqlalchemy import delete
 
-from db import RequestProduct
+from db import RequestProduct, Product
 from db.base import async_session_maker
 
 
 async def check():
     async with async_session_maker() as session:
         await session.execute(delete(RequestProduct))
+        await session.commit()
+    async with async_session_maker() as session:
+        await session.execute(delete(Product))
         await session.commit()
 
 
