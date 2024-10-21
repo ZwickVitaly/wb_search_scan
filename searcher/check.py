db254c16aa8747c6ead998a4163d08384f399145
zwickvitaly <zwickvitaly@gmail.com>
2024-10-21 15:03:34 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -7,13 +7,10 @@ from settings import logger
 
 async def check():
     async with async_session_maker() as session:
-        rqs = await session.execute(select(RequestProduct).filter(RequestProduct.products.contains([63730820])))
+        rqs = await session.execute(select(RequestProduct).order_by(func.cardinality(RequestProduct.products).desc()))
         result = rqs.scalars()
-        for r in result:
-            print(r.query, r.date, r.city)
-        rqs = await session.execute(select(Request).filter(Request.query == "вечная спичка огниво"))
-        result = rqs.scalars()
-        for r in result:
-            print(r.query)
+    for r in result:
+        if len(r.products) < 10:
+            logger.info(f"{r.query} - {r.products}")
 
 asyncio.run(check())
