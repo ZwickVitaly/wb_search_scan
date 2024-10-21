2a638424e22de3698c0dd05258e668eea13eb97c
zwickvitaly <zwickvitaly@gmail.com>
2024-10-21 15:01:24 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -9,8 +9,8 @@ async def check():
     async with async_session_maker() as session:
         rqs = await session.execute(select(RequestProduct).filter(RequestProduct.products.contains([63730820])))
         result = rqs.scalars()
-    for r in result:
-        print(r.query, r.date, r.city)
+        for r in result:
+            print(r.query, r.date, r.city)
         rqs = await session.execute(select(Request).filter(Request.query == "вечная спичка огниво"))
         result = rqs.scalars()
         for r in result:
