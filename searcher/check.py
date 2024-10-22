744690ce31238299db21e565d01038869b35f442
zwickvitaly <zwickvitaly@gmail.com>
2024-10-23 02:47:08 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -8,15 +8,15 @@ from settings import logger
 
 
 async def check():
-    id_set = set()
     prev = 0
     for i in range(0, 4000000, 10000):
+        id_set = set()
         async with async_session_maker() as session:
             res = await session.execute(select(RequestProduct).offset(prev).limit(i))
             res = res.scalars()
             for r in res:
                 id_set.update(r.products)
-    with open("products.bson", "wb+") as file:
-        json.dump(id_set, file, indent=2, ensure_ascii=False)
+            with open("products.bson", "ab+") as file:
+                json.dump(list(id_set), file, indent=2, ensure_ascii=False)
 
 asyncio.run(check())
