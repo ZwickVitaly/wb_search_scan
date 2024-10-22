04c204dd2786d08a88f83e06274cf9700ff2a526
zwickvitaly <zwickvitaly@gmail.com>
2024-10-22 17:53:09 +0300

    play

--- searcher/playground.py
+++ searcher/playground.py
@@ -1,16 +1,13 @@
 import asyncio
-import json
-import time
 from datetime import datetime
 from multiprocessing import Pool
-from aiofiles import open as aiopen
 from aiohttp import ClientSession
 from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import insert
 
 from parser.get_single_query_data import get_query_data
 from db.base import async_session_maker
-from db import City, Request, RequestProduct, Product
+from db import City, Request, RequestProduct
 from settings import logger
 
 
@@ -23,12 +20,11 @@ async def get_cities_data():
 
 async def get_requests_data():
     async with async_session_maker() as s:
-        q = await s.execute(select(Request).order_by(Request.quantity.desc()).limit(1000))
+        q = await s.execute(select(Request).order_by(Request.quantity.desc()))
         requests = q.scalars()
     return requests
 
 async def save_to_db(queue, model, update=False):
-    logger.critical("ЕЩЁ")
     while True:
         items = []
         item = None
@@ -45,11 +41,7 @@ async def save_to_db(queue, model, update=False):
             async with async_session_maker() as session:
                 if not update:
                     await session.execute(insert(model).values(items))
-                    logger.critical("УСПЕХ 1")
                 else:
-                    # stmt = insert(model)
-                    # excluded_fields = {col.name: stmt.excluded[col.name] for col in model.__table__.columns if
-                    #                    not col.primary_key}
                     primary_fields = [col.name for col in model.__table__.columns if col.primary_key]
                     try:
                         await session.execute(
@@ -59,7 +51,6 @@ async def save_to_db(queue, model, update=False):
                         )
                     except Exception as e:
                         logger.error(f"{e}")
-                    logger.critical("УСПЕХ!")
                 await session.commit()
         if item is None:
             await queue.put(item)
@@ -68,13 +59,13 @@ async def save_to_db(queue, model, update=False):
 
 
 
-async def get_r_data_q(queue: asyncio.Queue, city, date, http_session, product_queue=None, request_product_queue=None):
+async def get_r_data_q(queue: asyncio.Queue, city, date, http_session, request_product_queue=None):
     while True:
         r = await queue.get()
         if r is None:
             await queue.put(r)
             break
-        await get_r_data(r=r, city=city, date=date, http_session=http_session, product_queue=product_queue, request_product_queue=request_product_queue)
+        await get_r_data(r=r, city=city, date=date, http_session=http_session, request_product_queue=request_product_queue)
         queue.task_done()
 
 async def try_except_query_data(query_string, dest, limit, page, http_session, rqa=5):
@@ -92,7 +83,7 @@ async def try_except_query_data(query_string, dest, limit, page, http_session, r
         x = {"products": []}
     return x
 
-async def get_r_data(r, city, date, http_session, product_queue=None, request_product_queue=None):
+async def get_r_data(r, city, date, http_session, request_product_queue=None):
     while True:
         try:
             full_res = []
@@ -113,28 +104,6 @@ async def get_r_data(r, city, date, http_session, product_queue=None, request_pr
                 full_res.extend(res.get("products", []))
             if not full_res:
                 full_res = []
-            logger.critical("ПОЧТИ")
-            # seen = set()
-            products = []
-            for p in full_res:
-                # if p.get("id") not in seen:
-                products.append(
-                    {
-                        "wb_id": p.get("id"),
-                        "name": p.get("name"),
-                        "brand": p.get("brand"),
-                        "brandId": p.get("brandId"),
-                        "supplier": p.get("supplier"),
-                        "supplierId": p.get("supplierId"),
-                        "entity": p.get("entity"),
-                    }
-                )
-                #     seen.add(p.get("id"))
-                # seen.clear()
-            async with aiopen("dump.bson", "ab") as aiofile:
-                for prod in products:
-                    await aiofile.write(f"{json.dumps(prod, indent=2, ensure_ascii=False)},".encode())
-            logger.critical("ПОЧТИ 2")
             request_product = {
                 "city": city.id,
                 "query": r.query,
@@ -151,12 +120,11 @@ async def get_r_data(r, city, date, http_session, product_queue=None, request_pr
 
 async def get_city_result(city, date):
     requests = [r for r in await get_requests_data() if not r.query.isdigit()]
-#     logger.info(f"{city.name} start, {len(requests)}")
-#     product_queue = asyncio.Queue()
-    request_product_queue = asyncio.Queue(10)
-    workers_queue = asyncio.Queue(10)
-    # product_save_task = [asyncio.create_task(save_to_db(product_queue, Product, update=True)) for _ in range(20)]
-    request_product_save_task = [asyncio.create_task(save_to_db(request_product_queue, RequestProduct)) for _ in range(10)]
+    request_product_queue = asyncio.Queue()
+    workers_queue = asyncio.Queue()
+    request_product_save_task = [
+        asyncio.create_task(save_to_db(request_product_queue, RequestProduct)) for _ in range(5)
+    ]
     async with ClientSession() as http_session:
         requests_tasks = [
             asyncio.create_task(
@@ -165,7 +133,6 @@ async def get_city_result(city, date):
                     city=city,
                     date=date,
                     http_session=http_session,
-                    # product_queue=product_queue,
                     request_product_queue=request_product_queue
                 )
             ) for _ in range(20)
@@ -176,25 +143,16 @@ async def get_city_result(city, date):
         await asyncio.gather(*requests_tasks)
         await request_product_queue.put(None)
         await asyncio.gather(*request_product_save_task)
-#             logger.info(f"{city.name} BATCH {_}")
-#     logger.info(f"{city.name} complete")
 
 def run_pool_threads(func, *args, **kwargs):
-#     logger.info("Применен процесс")
     asyncio.run(func(*args, **kwargs))
 
-def get_results():
-    with open("dump.bson", "wb+") as file:
-        file.write(b"[\n")
-        pass
-    start = time.time()
+async def get_results():
+    start_time = datetime.now()
     logger.info("Вход в программу")
     today = datetime.now().date()
-    loop = asyncio.new_event_loop()
-    cities = loop.run_until_complete(get_cities_data())
-#     logger.info("Города есть")
+    cities = await get_cities_data()
     cities = list(cities)
-#     logger.info("Начало обхода")
     with Pool(len(cities)) as p:
         tasks = [
             p.apply_async(run_pool_threads, args=[get_city_result, city, today])
@@ -202,8 +160,10 @@ def get_results():
         ]
         p.close()
         p.join()
-    end = time.time()
-    logger.info(f"END TIME: {round(end - start, 2)}s")
-
-get_results()
-
+    end_time = datetime.now()
+    delta = (start_time - end_time).seconds
+    logger.info(
+        f"Старт парса: {start_time.strftime('%H:%M %d.%m.%Y')}\n"
+        f"Завершение парса: {end_time.strftime('%H:%M %d.%m.%Y')}\n"
+        f"Выполнено за: {delta // 60 // 60} часов, {delta // 60} минут"
+    )
