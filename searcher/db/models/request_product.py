913f432c21347595d1d2a9e96f3430fe468e572c
zwickvitaly <zwickvitaly@gmail.com>
2024-10-23 03:48:20 +0300

    play

--- searcher/db/models/request_product.py
+++ searcher/db/models/request_product.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from sqlalchemy import Integer, ForeignKey, Date, func, String
+from sqlalchemy import Integer, ForeignKey, Date, func, String, Index
 from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy.dialects.postgresql import ARRAY
 
@@ -9,6 +9,9 @@ from ..base import Base
 
 class RequestProduct(Base):
     __tablename__ = "request_product"
+    __table_args__ = (
+        Index('idx_request_product_products', "products", postgresql_using='gin'),
+    )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     city: Mapped[int] = mapped_column(ForeignKey('cities.id', ondelete="CASCADE"))
