a587bc24e9faa811df353e89d664dcaf104a340a
zwickvitaly <zwickvitaly@gmail.com>
2024-10-23 03:02:40 +0300

    play

--- searcher/check.py
+++ searcher/check.py
@@ -1,6 +1,7 @@
 import json
 
 from sqlalchemy import select, func
+from sqlalchemy.dialects.postgresql import array_agg
 import asyncio
 from db import RequestProduct, Request, Product
 from db.base import async_session_maker
@@ -8,15 +9,10 @@ from settings import logger
 
 
 async def check():
-    prev = 0
-    for i in range(0, 4000000, 1000):
-        id_set = set()
-        async with async_session_maker() as session:
-            res = await session.execute(select(RequestProduct).offset(prev).limit(i))
-            res = res.scalars()
-            for r in res:
-                id_set.update(r.products)
-            with open("products.bson", "ab+") as file:
-                file.write(f"{json.dumps(list(id_set))}\n".encode())
+    async with async_session_maker() as session:
+        res = await session.execute(select(func.sum(func.array_length(RequestProduct.products, 1))))
+        res = res.scalar()
+    logger.info(res)
+
 
 asyncio.run(check())
