b30ee279d20bcfcd478407a0baac75d501990307
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 13:49:20 +0300

    clickhouse

--- searcher/check.py
+++ searcher/check.py
@@ -1,30 +1,15 @@
 from sqlalchemy import select, func, delete
 import asyncio
-from db import RequestProduct, Request, Product
-from db.base import async_session_maker
+
+from clickhouse_db.get_async_connection import get_async_connection
 from settings import logger
 
 
 async def check():
-    async with async_session_maker() as session:
-        await session.execute(delete(RequestProduct))
-        subquery = (
-            select(func.unnest(RequestProduct.products).label('value'))
-            .order_by("value")
-            .distinct()
-            .subquery()
-        )
-
-        # Запрос для подсчета уникальных значений
-        result = await session.execute(select(func.count(subquery.c.value)))
-        unique_count = result.scalar()
-        logger.info(unique_count)
-
-
-        # Запрос для подсчета уникальных значений
-        result = await session.execute(select(func.count()).select_from(RequestProduct).where(RequestProduct.city == 1))
-        reqs = result.scalar()
-        print(reqs)
+    async with get_async_connection() as client:
+        query = "SELECT * FROM request_product;"
+        res = await client.query(query)
+        logger.info(res.result_rows)
 
 
 asyncio.run(check())
