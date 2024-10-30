671d9241b3d9c2970ff1833681c88ad0caa9abc3
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 19:37:49 +0300

    clickhouse

--- searcher/check.py
+++ searcher/check.py
@@ -9,12 +9,11 @@ from settings import logger
 async def check(searched_val, city):
     async with get_async_connection() as client:
         client: AsyncClient = client
-        query = f"""SELECT rp.query, r.quantity, groupArray((rp.date, indexOf(rp.products, {searched_val}) AS product_index)) AS products 
+        query = f"""SELECT rp.query, groupArray((rp.date, r.quantity, indexOf(rp.products, {searched_val}) AS product_index)) AS date_info 
         FROM request_product AS rp 
         JOIN request AS r ON r.query = rp.query 
         WHERE has(rp.products, {searched_val}) 
-        AND (rp.city = {city}) 
-        GROUP BY rp.query 
+        AND (rp.city = {city}) GROUP BY rp.query 
         ORDER BY rp.query;"""
         # query = f"SELECT rp.query, r.quantity FROM request_product as rp JOIN request AS r ON r.query = rp.query WHERE rp.city = {city} AND arrayExists(x -> x IN {searched_val}, rp.products) ORDER BY r.quantity DESC;"
         # query = f"SELECT city, count(*) FROM request_product GROUP BY city;"
