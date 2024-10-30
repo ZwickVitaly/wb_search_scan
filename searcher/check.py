f1b240af036bfd24324c53f67f39cb246c849ed8
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 19:51:27 +0300

    clickhouse

--- searcher/check.py
+++ searcher/check.py
@@ -9,7 +9,7 @@ from settings import logger
 async def check(searched_val, city):
     async with get_async_connection() as client:
         client: AsyncClient = client
-        query = f"""SELECT rp.query, groupArray((rp.date, r.quantity, indexOf(rp.products, {searched_val}) AS product_index)) AS date_info
+        query = f"""SELECT sd.query, groupArray((rp.date, r.quantity, indexOf(rp.products, {searched_val}) AS product_index)) AS date_info
         FROM (SELECT rp.query, rp.date, r.quantity, rp.products 
         FROM request_product AS rp JOIN
         request AS r ON r.query = rp.query 
@@ -17,9 +17,9 @@ async def check(searched_val, city):
         AND (rp.city = {city}) 
         AND (rp.date BETWEEN '2024-10-29' AND '2024-10-31')
         ORDER BY rp.date, r.quantity DESC
-        ) AS sorted_data 
-        GROUP BY rp.query 
-        ORDER BY rp.query;"""
+        ) AS sd 
+        GROUP BY sd.query 
+        ORDER BY sd.query;"""
         # query = f"SELECT rp.query, r.quantity FROM request_product as rp JOIN request AS r ON r.query = rp.query WHERE rp.city = {city} AND arrayExists(x -> x IN {searched_val}, rp.products) ORDER BY r.quantity DESC;"
         # query = f"SELECT city, count(*) FROM request_product GROUP BY city;"
         res = await client.query(query)
