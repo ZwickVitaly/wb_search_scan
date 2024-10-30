c8c348b238d7591fc178793572e0b26ae3e19f5e
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 19:39:57 +0300

    clickhouse

--- searcher/check.py
+++ searcher/check.py
@@ -14,6 +14,7 @@ async def check(searched_val, city):
         JOIN request AS r ON r.query = rp.query 
         WHERE has(rp.products, {searched_val}) 
         AND (rp.city = {city}) GROUP BY rp.query 
+        AND (rp.date BETWEEN '2024-10-29' AND '2024-10-31')  
         ORDER BY rp.query;"""
         # query = f"SELECT rp.query, r.quantity FROM request_product as rp JOIN request AS r ON r.query = rp.query WHERE rp.city = {city} AND arrayExists(x -> x IN {searched_val}, rp.products) ORDER BY r.quantity DESC;"
         # query = f"SELECT city, count(*) FROM request_product GROUP BY city;"
