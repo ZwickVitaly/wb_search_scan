ab2e183cbb85c22d40784aaadca05864c6fd0659
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 21:28:15 +0300

    clickhouse

--- searcher/main.py
+++ searcher/main.py
@@ -1,6 +1,3 @@
 from server.server_main import app
-import uvicorn
 
 app = app
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=9013, log_level="info", workers=4, reload=True)
