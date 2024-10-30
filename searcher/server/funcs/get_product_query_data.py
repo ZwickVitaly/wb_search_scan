d408de0132e9f1f74351c9711e7121a0abce00c0
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 20:38:07 +0300

    clickhouse

--- searcher/server/funcs/get_product_query_data.py
+++ searcher/server/funcs/get_product_query_data.py
@@ -9,21 +9,27 @@ async def get_db_data(product_id, dates_list, city):
         query = f"""SELECT sd.query, sd.quantity, groupArray((sd.date, indexOf(sd.products, {product_id}) AS product_index)) AS date_info
         FROM (SELECT rp.query, rp.date, r.quantity, rp.products
         FROM request_product AS rp
-        JOIN request AS r ON r.query = rp.query
+        JOIN (SELECT * FROM request FINAL) AS r ON r.query = rp.query
         WHERE has(rp.products, {product_id})
         AND (rp.city = {city})
         AND (rp.date BETWEEN '{min_date.strftime("%Y-%m-%d")}' AND '{max_date.strftime("%Y-%m-%d")}')
         ORDER BY rp.date, r.quantity DESC
         ) AS sd
-        GROUP BY sd.query
-        ORDER BY sd.quantity, sd.query;"""
+        GROUP BY sd.query, sd.quantity
+        ORDER BY sd.quantity DESC, sd.query;"""
         query_result = await client.query(query)
         result = [
             {
                 "query": row[0],
                 "quantity": row[1],
+                "dates": {
+                    str(j_row[0]): j_row[1]
+                    for j_row in row[3]
+                }
             }
+            for row in query_result.result_rows
         ]
+        return result
 
 
 
