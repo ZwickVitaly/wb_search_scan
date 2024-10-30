22c3fa2292c6c6ae7fc917003e869f90c1b8d4ab
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 19:33:17 +0300

    clickhouse

--- searcher/check.py
+++ searcher/check.py
@@ -9,18 +9,18 @@ from settings import logger
 async def check(searched_val, city):
     async with get_async_connection() as client:
         client: AsyncClient = client
-        query = f"""SELECT rp.date, groupArray((rp.query, r.quantity, indexOf(rp.products, {searched_val}) AS product_index)) AS products 
+        query = f"""SELECT rp.query, r.quantity, groupArray((rp.date, indexOf(rp.products, {searched_val}) AS product_index)) AS products 
         FROM request_product AS rp 
         JOIN request AS r ON r.query = rp.query 
         WHERE has(rp.products, {searched_val}) 
         AND (rp.city = {city}) 
-        GROUP BY rp.date 
+        GROUP BY rp.query 
         ORDER BY rp.date;"""
         # query = f"SELECT rp.query, r.quantity FROM request_product as rp JOIN request AS r ON r.query = rp.query WHERE rp.city = {city} AND arrayExists(x -> x IN {searched_val}, rp.products) ORDER BY r.quantity DESC;"
         # query = f"SELECT city, count(*) FROM request_product GROUP BY city;"
         res = await client.query(query)
-        json_result = [{"date": str(row[0]), "products": row[1]} for row in res.result_rows]
-        logger.info(json_result)
+        # json_result = [{"date": str(row[0]), "products": row[1]} for row in res.result_rows]
+        logger.info(res)
 
 
 asyncio.run(check(212296429, -1257786))
