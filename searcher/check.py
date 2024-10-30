988d3a74d2ed97d36cb082c4e1e175a168de2633
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 14:22:00 +0300

    clickhouse

--- searcher/check.py
+++ searcher/check.py
@@ -6,7 +6,7 @@ from settings import logger
 
 async def check():
     async with get_async_connection() as client:
-        query = "SELECT query FROM request_product WHERE has(products, 213950484);"
+        query = "SELECT query FROM request_product WHERE has(products, 213950484) AND city = -1257786;"
         res = await client.query(query)
         logger.info(res.result_rows)
 
