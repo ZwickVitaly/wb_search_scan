431be26605a6346ec5b4d5fe0a79c92fd6b8c43d
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 20:23:01 +0300

    clickhouse

--- searcher/check.py
+++ searcher/check.py
@@ -9,19 +9,19 @@ from settings import logger
 async def check(searched_val, city):
     async with get_async_connection() as client:
         client: AsyncClient = client
-        # query = f"""SELECT sd.query, groupArray((sd.date, sd.quantity, indexOf(sd.products, {searched_val}) AS product_index)) AS date_info
-        # FROM (SELECT rp.query, rp.date, r.quantity, rp.products
-        # FROM request_product AS rp
-        # JOIN request AS r ON r.query = rp.query
-        # WHERE has(rp.products, {searched_val})
-        # AND (rp.city = {city})
-        # AND (rp.date BETWEEN '2024-10-29' AND '2024-10-31')
-        # ORDER BY rp.date, r.quantity DESC
-        # ) AS sd
-        # GROUP BY sd.query
-        # ORDER BY sd.query;"""
+        query = f"""SELECT sd.query, sd.quantity, groupArray((sd.date, indexOf(sd.products, {searched_val}) AS product_index)) AS date_info
+        FROM (SELECT rp.query, rp.date, r.quantity, rp.products
+        FROM request_product AS rp
+        JOIN request AS r ON r.query = rp.query
+        WHERE has(rp.products, {searched_val})
+        AND (rp.city = {city})
+        AND (rp.date BETWEEN '2024-10-29' AND '2024-10-31')
+        ORDER BY rp.date, r.quantity DESC
+        ) AS sd
+        GROUP BY sd.query
+        ORDER BY sd.quantity, sd.query;"""
         # query = f"SELECT rp.query, r.quantity FROM request_product as rp JOIN request AS r ON r.query = rp.query WHERE rp.city = {city} AND arrayExists(x -> x IN {searched_val}, rp.products) ORDER BY r.quantity DESC;"
-        query = f"SELECT city, count(*) FROM request_product GROUP BY city;"
+        # query = f"SELECT city, count(*) FROM request_product GROUP BY city;"
         res = await client.query(query)
         # json_result = [{"date": str(row[0]), "products": row[1]} for row in res.result_rows]
         logger.info(res.result_rows)
--- searcher/server/funcs/get_product_query_data.py
+++ searcher/server/funcs/get_product_query_data.py
@@ -1,21 +1,29 @@
 from clickhouse_db.get_async_connection import get_async_connection
+from datetime import date
 
 
-async def check(searched_val, city):
+async def get_db_data(product_id, dates_list, city):
+    max_date: date = max(dates_list)
+    min_date: date = min(dates_list)
     async with get_async_connection() as client:
-        query = f"""SELECT
-                rp.date,
-                rp.query,
-                r.quantity,
-                indexOf(rp.products, {searched_val}) AS product_index
-            FROM
-                request_product AS rp
-            JOIN
-                request AS r ON r.query = rp.query
-            WHERE
-                has(rp.products, {searched_val})
-            AND
-                (rp.city = {city})
-            ORDER BY
-            r.quantity DESC;"""
-        res = await client.query(query)
+        query = f"""SELECT sd.query, sd.quantity, groupArray((sd.date, indexOf(sd.products, {product_id}) AS product_index)) AS date_info
+        FROM (SELECT rp.query, rp.date, r.quantity, rp.products
+        FROM request_product AS rp
+        JOIN request AS r ON r.query = rp.query
+        WHERE has(rp.products, {product_id})
+        AND (rp.city = {city})
+        AND (rp.date BETWEEN '{min_date.strftime("%Y-%m-%d")}' AND '{max_date.strftime("%Y-%m-%d")}')
+        ORDER BY rp.date, r.quantity DESC
+        ) AS sd
+        GROUP BY sd.query
+        ORDER BY sd.quantity, sd.query;"""
+        query_result = await client.query(query)
+        result = [
+            {
+                "query": row[0],
+                "quantity": row[1],
+            }
+        ]
+
+
+
