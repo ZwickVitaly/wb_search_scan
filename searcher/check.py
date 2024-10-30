be33a24b6939eec07de19f59e1e4d3f93e7cbde6
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 16:46:56 +0300

    clickhouse

--- searcher/check.py
+++ searcher/check.py
@@ -8,7 +8,7 @@ async def check(searched_val):
     async with get_async_connection() as client:
         query = f"SELECT date, query, indexOf(products, {searched_val}) AS product_index FROM request_product WHERE has(products, {searched_val}) AND city = -1257786 ORDER BY product_index;"
         res = await client.query(query)
-        logger.info(f"{'\n'.join([str(str(date), query, product_index) for date, query, product_index in res.result_rows])}")
+        logger.info(f"{'\n'.join([str((str(date), query, product_index)) for date, query, product_index in res.result_rows])}")
 
 
 asyncio.run(check(212296429))
