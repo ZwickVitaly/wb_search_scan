2ec24edb04ea9a1ab193d3033fc07630ad3aa885
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 18:11:59 +0300

    clickhouse

--- searcher/clickhouse_db/get_async_connection.py
+++ searcher/clickhouse_db/get_async_connection.py
@@ -6,7 +6,7 @@ from settings import CLICKHOUSE_CONFING
 
 @asynccontextmanager
 async def get_async_connection() -> AsyncClient:
-    session = AsyncSession(**CLICKHOUSE_CONFING)
+    session = AsyncSession(CLICKHOUSE_CONFING)
     async with session as client:
         yield client
 
@@ -17,7 +17,7 @@ class AsyncSession:
         self.config = clickhouse_config
 
     async def __aenter__(self):
-        self.client = await clickhouse_connect.get_async_client(**CLICKHOUSE_CONFING)
+        self.client = await clickhouse_connect.get_async_client(**self.config)
         return self.client
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
